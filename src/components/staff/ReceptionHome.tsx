2b9d22ff6f15e500c715847d9832476e0fcf307e
Palawan Collective <growpalawan@gmail.com>
2026-04-07 07:41:46 +0800

    Simplify ReceptionHome by removing DailyBriefing
    Removed DailyBriefing component from ReceptionHome.

--- src/components/staff/ReceptionHome.tsx
+++ src/components/staff/ReceptionHome.tsx
@@ -1,14 +1,7 @@
-import { useNavigate } from 'react-router-dom';
 import ReceptionPage from '@/pages/ReceptionPage';
-import { DailyBriefing } from './DailyBriefing';
 
 const ReceptionHome = () => {
-  return (
-    <>
-      <DailyBriefing />
-      <ReceptionPage embedded />
-    </>
-  );
+  return <ReceptionPage embedded />;
 };
 
 export default ReceptionHome;
